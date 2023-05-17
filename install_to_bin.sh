4d2eb87aa1449ede8010ec15019e9db0ba3464fe
ethmth <ethan@smaze.net>
2023-05-16 20:20:57 -0400

    Change script to executable in install_to_bin.sh script

--- install_to_bin.sh
+++ install_to_bin.sh
@@ -7,9 +7,11 @@ while IFS= read -r line; do
 	if grep -q "home" <<< "$line"; then
         echo "Copying $SCRIPT_NAME to $line$SCRIPT_NAME"
 		cp $SCRIPT_NAME $line$SCRIPT_NAME
+		chmod +x $line$SCRIPT_NAME
     else
         echo "Copying $SCRIPT_NAME to $line$SCRIPT_NAME as root"
 		sudo -k cp $SCRIPT_NAME $line$SCRIPT_NAME
+		sudo chmod +x $line$SCRIPT_NAME
     fi
 done < "$LOCATIONS_FILE"
 
