9859e7252b97071041634def10cc30f0afed5963
ethmth <ethan@smaze.net>
2023-05-16 20:19:22 -0400

    Created install_to_bin.sh script to install the script to user-specified location from vars/install_location.conf

---
+++ install_to_bin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LOCATIONS_FILE="vars/install_location.conf"
+SCRIPT_NAME="vpn"
+
+while IFS= read -r line; do
+	if grep -q "home" <<< "$line"; then
+        echo "Copying $SCRIPT_NAME to $line$SCRIPT_NAME"
+		cp $SCRIPT_NAME $line$SCRIPT_NAME
+    else
+        echo "Copying $SCRIPT_NAME to $line$SCRIPT_NAME as root"
+		sudo -k cp $SCRIPT_NAME $line$SCRIPT_NAME
+    fi
+done < "$LOCATIONS_FILE"
+
+rm $SCRIPT_NAME
--- set_vars.sh
+++ set_vars.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-VARS_FILE="vars.conf"
+VARS_FILE="vars/vars.conf"
 SCRIPT_FILE="vpn.sh"
+NEW_SCRIPT="vpn"
+
+cp $SCRIPT_FILE $NEW_SCRIPT
 
 while IFS= read -r line; do
     key="${line%%=*}"
-	sed -i "/$key=/c\\$line" $SCRIPT_FILE
+	sed -i "/$key=/c\\$line" $NEW_SCRIPT
 done < "$VARS_FILE"
 
-echo "Updated variables in $SCRIPT_FILE from $VARS_FILE"
+echo "Created script file $NEW_SCRIPT from $SCRIPT_FILE with variables from $VARS_FILE"
